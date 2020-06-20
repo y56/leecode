9c9182227d352f31811bdbde773d10a02955eaca
Eugene Wang <36190560+y56@users.noreply.github.com>
2020-06-20 23:55:20 +0800

    Update 269. Alien Dictionary.py

--- 269. Alien Dictionary.py
+++ 269. Alien Dictionary.py
@@ -29,3 +29,39 @@ class Solution:
         if len(ans) < len(in_degree):
             return ""   
         return ''.join(ans)
+"""
+DFS
+"""
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        reversed_adj_list = {c : [] for word in words for c in word}
+        
+        for first_word, second_word in zip(words,words[1::]):
+            for c,d in zip(first_word,second_word):
+                if c!=d:
+                    reversed_adj_list[d].append(c) #  c < d
+                    break
+            else:
+                if len(first_word)>len(second_word):
+                    return ''
+                
+        seen={} # once and twice
+        ans=[]
+        def dfs(node):
+            if node in seen:
+                if seen[node]=="grey":
+                    return False # we detect a loop
+                else: # black
+                    return True
+            seen[node]="grey"
+            for next_node in reversed_adj_list[node]:
+                # all next_node < node
+                if dfs(next_node)==False: # we detect a loop
+                    return False
+            seen[node]="black"
+            ans.append(node)
+            return True
+        
+        if not all(dfs(node) for node in reversed_adj_list):
+            return ''
+        return ''.join(ans)
